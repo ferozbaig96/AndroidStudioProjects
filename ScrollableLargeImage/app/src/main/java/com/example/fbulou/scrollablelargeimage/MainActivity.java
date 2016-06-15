2f889d8c45e131ff674b9e56efb567eea96eb753
Mohd Feroz Baig <feroz.baig96@gmail.com>
2016-06-15 15:35:23 +0530

    Initial Commit

---
+++ ScrollableLargeImage/app/src/main/java/com/example/fbulou/scrollablelargeimage/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.fbulou.scrollablelargeimage;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    int screenWidth, screenHeight;
+    int imageWidth, imageHeight;
+    //Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       /* toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        image = (ImageView) findViewById(R.id.image);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        scrollImage();
+
+    }
+
+    private void scrollImage() {
+
+        //Get the image dimensions
+        imageWidth = image.getDrawable().getIntrinsicWidth();
+        imageHeight = image.getDrawable().getIntrinsicHeight();
+
+
+        // set maximum scroll amount (based on center of image)
+        int maxX = (imageWidth - screenWidth) / 2;
+        //  int maxY = (imageHeight - screenHeight + toolbar.getHeight()) / 2;
+        int maxY = (imageHeight - screenHeight) / 2;
+
+        // set scroll limits
+        final int maxLeft = (maxX * -1);
+        final int maxRight = maxX;
+        final int maxTop = (maxY * -1);
+        final int maxBottom = maxY;
+
+        // set touchListener
+        image.setOnTouchListener(new View.OnTouchListener() {
+            float downX, downY;
+            int totalX, totalY;
+            int scrollByX, scrollByY;
+
+            public boolean onTouch(View view, MotionEvent event) {
+                float currentX, currentY;
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        downX = event.getX();
+                        downY = event.getY();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        currentX = event.getX();
+                        currentY = event.getY();
+                        scrollByX = (int) (downX - currentX);
+                        scrollByY = (int) (downY - currentY);
+
+                        // scrolling to left side of image (pic moving to the right)
+                        if (currentX > downX) {
+                            if (totalX == maxLeft) {
+                                scrollByX = 0;
+                            }
+                            if (totalX > maxLeft) {
+                                totalX = totalX + scrollByX;
+                            }
+                            if (totalX < maxLeft) {
+                                scrollByX = maxLeft - (totalX - scrollByX);
+                                totalX = maxLeft;
+                            }
+                        }
+
+                        // scrolling to right side of image (pic moving to the left)
+                        if (currentX < downX) {
+                            if (totalX == maxRight) {
+                                scrollByX = 0;
+                            }
+                            if (totalX < maxRight) {
+                                totalX = totalX + scrollByX;
+                            }
+                            if (totalX > maxRight) {
+                                scrollByX = maxRight - (totalX - scrollByX);
+                                totalX = maxRight;
+                            }
+                        }
+
+                        // scrolling to top of image (pic moving to the bottom)
+                        if (currentY > downY) {
+                            if (totalY == maxTop) {
+                                scrollByY = 0;
+                            }
+                            if (totalY > maxTop) {
+                                totalY = totalY + scrollByY;
+                            }
+                            if (totalY < maxTop) {
+                                scrollByY = maxTop - (totalY - scrollByY);
+                                totalY = maxTop;
+                            }
+                        }
+
+                        // scrolling to bottom of image (pic moving to the top)
+                        if (currentY < downY) {
+                            if (totalY == maxBottom) {
+                                scrollByY = 0;
+                            }
+                            if (totalY < maxBottom) {
+                                totalY = totalY + scrollByY;
+                            }
+                            if (totalY > maxBottom) {
+                                scrollByY = maxBottom - (totalY - scrollByY);
+                                totalY = maxBottom;
+                            }
+                        }
+
+                        image.scrollBy(scrollByX, scrollByY);
+                        downX = currentX;
+                        downY = currentY;
+                        break;
+
+                }
+
+                return true;
+            }
+        });
+    }
+
+    //MENUS
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
