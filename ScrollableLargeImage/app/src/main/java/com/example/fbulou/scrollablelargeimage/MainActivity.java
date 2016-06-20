631b2e9a5ef0b857156d34688f12a30029617b94
Mohd Feroz Baig <feroz.baig96@gmail.com>
2016-06-20 16:57:40 +0530

    Initial Commit

--- ScrollableLargeImage/app/src/main/java/com/example/fbulou/scrollablelargeimage/MainActivity.java
+++ ScrollableLargeImage/app/src/main/java/com/example/fbulou/scrollablelargeimage/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fbulou.scrollablelargeimage;
 
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.Menu;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //WORKS FINE
 
     ImageView image;
     int screenWidth, screenHeight;
@@ -24,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
        /* toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
 
-/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,10 +35,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                image.setImageDrawable(getResources().getDrawable(R.drawable.wood));        //can use setImageBitmap as well
+                scrollImage();
+                image.scrollTo(0, 0);
             }
-        });*/
+        });
 
         image = (ImageView) findViewById(R.id.image);
+
     }
 
     @Override
@@ -57,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         //Get the image dimensions
         imageWidth = image.getDrawable().getIntrinsicWidth();
         imageHeight = image.getDrawable().getIntrinsicHeight();
-
 
         // set maximum scroll amount (based on center of image)
         int maxX = (imageWidth - screenWidth) / 2;
