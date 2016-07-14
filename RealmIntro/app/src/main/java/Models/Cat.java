1df6fdfa38e87f8bb12c166a94762a35e05cfe14
Mohd Feroz Baig <feroz.baig96@gmail.com>
2016-07-14 16:43:08 +0530

    Initial commit

---
+++ RealmIntro/app/src/main/java/Models/Cat.java
@@ -0,0 +1,7 @@
+package Models;
+
+import io.realm.RealmObject;
+
+public class Cat extends RealmObject {
+    public String name;
+}
---
+++ RealmIntro/app/src/main/java/Models/Dog.java
@@ -0,0 +1,7 @@
+package Models;
+
+import io.realm.RealmObject;
+
+public class Dog extends RealmObject {
+    public String name;
+}
