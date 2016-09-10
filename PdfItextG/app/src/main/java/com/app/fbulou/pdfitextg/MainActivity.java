8c3b8a01275f9e1a49f0a80852be48bdd6f91812
Mohd Feroz Baig <feroz.baig96@gmail.com>
2016-09-10 18:02:07 +0530

    Initial Commit

---
+++ PdfItextG/app/src/main/java/com/app/fbulou/pdfitextg/MainActivity.java
@@ -0,0 +1,175 @@
+package com.app.fbulou.pdfitextg;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+
+    EditText mSubjectEditText;
+    EditText mBodyEditText;
+    Button mSaveButton;
+
+    File myFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Add the permission request in AndroidManifest,xml
+
+        initialise();
+    }
+
+    private void initialise() {
+        mSubjectEditText = (EditText) findViewById(R.id.edit_text_subject);
+        mBodyEditText = (EditText) findViewById(R.id.edit_text_body);
+        mSaveButton = (Button) findViewById(R.id.button_save);
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSubjectEditText.getText().toString().isEmpty()) {
+                    mSubjectEditText.setError("Subject is empty");
+                    mSubjectEditText.requestFocus();
+                    return;
+                }
+
+                if (mBodyEditText.getText().toString().isEmpty()) {
+                    mBodyEditText.setError("Body is empty");
+                    mBodyEditText.requestFocus();
+                    return;
+                }
+
+                try {
+                    createPdf();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    //Creating PDF in Android
+    private void createPdf() throws FileNotFoundException, DocumentException {
+
+        File pdfFolder = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/pdfdemo");
+        if (!pdfFolder.exists()) {
+            pdfFolder.mkdir();
+            Log.i(TAG, "Pdf Directory created");
+        }
+
+        //Create time stamp
+        Date date = new Date();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(date);
+
+        myFile = new File(pdfFolder + timeStamp + ".pdf");
+
+        OutputStream output = new FileOutputStream(myFile);
+
+        //Step 1
+        Document document = new Document();
+
+       /* Rectangle pagesize = new Rectangle(216f, 720f);
+        Document document = new Document(pagesize, 36f, 72f, 108f, 180f);*/
+
+        //Step 2: Get an Instance of PdfWriter
+        PdfWriter.getInstance(document, output);
+
+        //Step 3: Open the PDF Document
+        document.open();
+
+        //Step 4
+        document.add(new Paragraph(mSubjectEditText.getText().toString()));
+        document.add(new Paragraph(mBodyEditText.getText().toString()));
+
+        //Step 5: Close the document
+        document.close();
+
+        promptForNextAction();
+
+    }
+
+    //Viewing PDF in Android
+    private void viewPdf() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(myFile), "application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
+
+    //Email PDF Attachment in Android
+    private void emailNote() {
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_SUBJECT, mSubjectEditText.getText().toString());
+        email.putExtra(Intent.EXTRA_TEXT, mBodyEditText.getText().toString());
+        Uri uri = Uri.parse(myFile.getAbsolutePath());
+        email.putExtra(Intent.EXTRA_STREAM, uri);
+        email.setType("message/rfc822");
+        startActivity(email);
+    }
+
+    //Option of either sending the the PDF as email or just vieweing on the screen
+    private void promptForNextAction() {
+        final String[] options = {getString(R.string.label_email), getString(R.string.label_preview),
+                getString(R.string.label_cancel)};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Note Saved, What Next?");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (options[which].equals(getString(R.string.label_email))) {
+                    emailNote();
+                } else if (options[which].equals(getString(R.string.label_preview))) {
+                    viewPdf();
+                } else if (options[which].equals(getString(R.string.label_cancel))) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        builder.show();
+
+    }
+
+}
