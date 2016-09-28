e4f4496998d20d52674902b74d0258e4fdbd4844
Mohd Feroz Baig <feroz.baig96@gmail.com>
2016-09-28 12:16:41 +0530

    Added Edittext for input with textwatcher

--- AutofitTextviewExample/app/src/main/java/com/app/fbulou/autofittextviewexample/MainActivity.java
+++ AutofitTextviewExample/app/src/main/java/com/app/fbulou/autofittextviewexample/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mAutofitOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        mAutofitOutput = (TextView) findViewById(R.id.output_autofit);
+
+        ((EditText) findViewById(R.id.input_edittext)).addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mAutofitOutput.setText(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //do nothing
             }
         });
+
     }
 
 
