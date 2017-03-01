220be1685e6ca4cc46a450659b083c32f23c7065
Mohd Feroz Baig <feroz.baig96@gmail.com>
2017-03-01 14:17:30 +0530

    Initial commit

---
+++ AppIntro-MyBitutorial/app/src/main/java/com/app/fbulou/appintro_mybitutorial/MainActivity.java
@@ -0,0 +1,30 @@
+package com.app.fbulou.appintro_mybitutorial;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+   /*
+        https://github.com/Cleveroad/Bitutorial
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, SimpleViewsFragment.instance())
+                    .commit();
+        }
+    }
+
+}
